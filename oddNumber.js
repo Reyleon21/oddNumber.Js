eb92801adc6b31b7b7463f6615c07844234daaaa
Reyleon21 <69471558+Reyleon21@users.noreply.github.com>
2020-12-03 10:27:49 -0600

    Add files via upload

---
+++ oddNumber.js
@@ -0,0 +1,12 @@
+///Write a program to find the odd numbers from 1 to 1000
+
+var number = 1;
+
+while(number<=1000)
+{
+  if(number % 2 !=0)
+    {
+      console.log(number);
+    }
+  number = number +1;
+}
